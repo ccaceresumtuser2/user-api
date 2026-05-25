fef5a7bb7675df75f6d3af36f526c5018758c5f1
Carlos Jose Caceres Ochoa <jcacereso@unicartagena.edu.co>
2026-05-25 13:50:30 -0500

    Nueva clase PersonaPortIn

---
+++ src/main/java/com/empresa/api/application/port/in/PersonaPortIn.java
@@ -0,0 +1,16 @@
+package com.empresa.api.application.port.in;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.empresa.api.domain.model.User;
+
+public interface PersonaPortIn {
+    String createUser(User userData);
+    List<User> findAll();
+    Optional<User> findByEmail(String email);
+    List<User> buscarPorNombre(String nombre);
+    List<User> buscarPorApellido(String apellido);
+    List<User> buscarPorEdad(String edad);
+    long count();
+}
